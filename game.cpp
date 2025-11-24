a9bab393287e9b28c36b4a7754118758a596c092
sava8sava <supremomelao@gmail.com>
2025-11-24 14:47:07 -0300

    limitei o fps

--- game.cpp
+++ game.cpp
@@ -210,7 +210,8 @@ void fps_counter(){
 
 void calculate_delta(){
   float curr_delta = glutGet(GLUT_ELAPSED_TIME);
-  delta = (curr_delta - prev_delta)/1000.0f; 
+  delta = (curr_delta - prev_delta)/1000.0f;
+  if(delta < 0.0033f) delta = 0.0033f; //limita o jogo a 300fps
   prev_delta = curr_delta;
 }
 
